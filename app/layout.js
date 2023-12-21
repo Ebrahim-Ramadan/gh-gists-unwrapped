920ca4cfe0b895733af76cf7bcebcfb764803c0e
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-12-21 13:54:35 +0200

    added animation on landing cat, added not-found, added metadata

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+         
+      <meta name="description" content="Jr. software dev" />
+        <meta data-n-head="ssr" data-hid="og:image:type" property="og:image:type" content="image/jpeg"/>
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+        <title>gh.gists Unwrapped</title> 
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
